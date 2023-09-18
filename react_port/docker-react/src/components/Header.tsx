a555b41f78e4cbac0409394fa6d29cff29809ed7
oackland <oackland.toro@outlook.com>
2023-09-18 04:28:10 +0000

    aded component

---
+++ react_port/docker-react/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import HighlightIcon from "@mui/icons-material/Highlight";
+const Header = () => {
+    return (
+        <header>
+            <h1>
+                <HighlightIcon/> ToDoList
+            </h1>
+        </header>
+            );
+};
+export default Header;
