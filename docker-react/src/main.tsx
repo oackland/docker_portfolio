66ed637983ecfd6dd999b83eabbe682e3e44eb75
OacklandToro <82695290+oackland@users.noreply.github.com>
2023-09-18 15:21:31 -0600

    Delete docker-react directory

--- docker-react/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; // Fixed the import path
-import App from './App'; // Removed the .tsx extension
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-);
